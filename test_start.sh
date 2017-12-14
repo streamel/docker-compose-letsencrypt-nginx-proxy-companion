9cb8820faa6a24f5ac0df3526b23c69aee877d0a
Evert Ramos <evert.ramos@gmail.com>
2017-12-14 12:20:00 -0500

    FastCGI test

--- test_start.sh
+++ test_start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#CONTAINER=httpd:alpine
+#CONTAINER=huggla/alpine-lighttpd2-fastcgi
+CONTAINER=nfqlt/apache24-fastcgi
+
 # Set up your DOMAIN
 if [ $# -eq 0 ]; then
     echo "Please inform your domain name to test your proxy."
@@ -14,9 +18,12 @@ source .env
 
 # Testing your proxy
 if [ -z ${SERVICE_NETWORK+X} ]; then
-    docker run -d -e VIRTUAL_HOST=$DOMAIN --network=$NETWORK --name test-web httpd:alpine
+    docker run -d -e VIRTUAL_HOST=$DOMAIN --network=$NETWORK --name test-web $CONTAINER
 else
-    docker run -d -e VIRTUAL_HOST=$DOMAIN --network=$SERVICE_NETWORK --name test-web httpd:alpine
+    #docker run -d -e VIRTUAL_HOST=$DOMAIN --network=$SERVICE_NETWORK --name test-web $CONTAINER
+    docker run -d -e VIRTUAL_HOST=$DOMAIN -e VIRTUAL_ROOT=/home/project/src/www/web --network=$NETWORK --name test-web $CONTAINER
+    #docker run -d -P --network=$NETWORK --name test-web $CONTAINER
+    echo 'ups!'
 fi
 
 exit 0
